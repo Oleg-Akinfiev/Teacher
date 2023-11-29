fd06b47412f98dc5b7d1b0a96bdf1e64170c25a0
Oleg Akinfiev <akinfiev91@gmail.com>
2023-11-29 20:36:15 +0300

    commit

---
+++ src/components/Form/FormCall.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormCall = () => {
+  return (
+    <div>FormCall</div>
+  )
+}
+
+export default FormCall
---
+++ src/components/Methodical/Methodical.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Methodical = () => {
+  return (
+    <div>Methodical</div>
+  )
+}
+
+export default Methodical
