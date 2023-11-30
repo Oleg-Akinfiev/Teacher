2dbbc702fd489ac1104c12f51272e9cd6a6d3b04
Oleg Akinfiev <akinfiev91@gmail.com>
2023-11-30 22:17:41 +0300

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,20 @@
+import Search from "../Search/Search";
+import "../styles/variables.scss";
 
 const Header = () => {
   return (
-    <div className='header'>
-        <div className="container">
-            <div>Главная</div>
-            <div>Расписание</div>
-            <div>Методические указания</div>
-            <div>Блог</div>
+      <div className="container">
+        <div className='header'>
+
+          <div className="header__title">
+            <a>Главная</a>
+            <a>Расписание</a>
+            <a>Методические указания</a>
+            <a>Блог</a>
+          </div>
+
+           <Search />
+
         </div>
     </div>
   )
