07e2d07a349051998801cf934f7ce2d1dfcbccdb
Oleg-Akinfiev <akinfiev91@gmail.com>
2024-04-09 14:48:39 +0300

    commit

--- src/components/Class/Class.jsx
+++ src/components/Class/Class.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-import school from "../Images/school.jpg";
-import school2 from "../Images/school2.jpg";
-import school3 from "../Images/school3.jpg";
-import school4 from "../Images/school4.jpg";
-import school5 from "../Images/school5.jpg";
+import class1 from "../Images/class1.jpg";
+import class2 from "../Images/class2.jpg";
+import class3 from "../Images/class3.jpg";
+import class4 from "../Images/class4.jpg";
+import class5 from "../Images/class5.jpg";
 
 const Class = () => {
   return (
@@ -12,11 +12,11 @@ const Class = () => {
       <div className="container">
         <div className="class__title">Как проходит обучение в классе</div>
         <div className="class__img">
-          <img className="one" src={school} alt="" />
-          <img src={school2} alt="" />
-          <img src={school3} alt="" />
-          <img src={school4} alt="" />
-          <img src={school5} alt="" />
+          <img className="one" src={class1} alt="" />
+          <img src={class2} alt="" />
+          <img src={class3} alt="" />
+          <img src={class4} alt="" />
+          <img src={class5} alt="" />
         </div>
       </div>
     </div>
--- src/components/Methodical/Methodical.jsx
+++ src/components/Methodical/Methodical.jsx
@@ -1,10 +1,34 @@
 import React from "react";
-import Class from "../Class/Class";
+
+import meth1 from "../Images/meth1.jpg";
+import meth2 from "../Images/meth2.jpg";
+import meth3 from "../Images/meth3.jpg";
 
 const Methodical = () => {
   return (
-    <div>
-      <div className="container">Методические указания</div>
+    <div className="methodical">
+      <div className="container">
+        <div className="methodical__intro">
+          <a href="#">
+            <div className="methodical__img">
+              <img src={meth1} alt="" />
+            </div>
+            <div className="methodical__title">Развитие ребенка</div>
+          </a>
+          <a href="#">
+            <div className="methodical__img">
+              <img src={meth2} alt="" />
+            </div>
+            <div className="methodical__title">Воспитание ребенка</div>
+          </a>
+          <a href="#">
+            <div className="methodical__img">
+              <img src={meth3} alt="" />
+            </div>
+            <div className="methodical__title">Школьные вопросы</div>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
